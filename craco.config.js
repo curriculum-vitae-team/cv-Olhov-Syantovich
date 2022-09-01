72a5e2047851377618fd687646bf6a864524e49b
Oliver Tears <olivertears@gmail.com>
2022-09-01 18:42:28 +0300

    fix: folder structure & dependencies location

--- craco.config.js
+++ craco.config.js
@@ -4,13 +4,16 @@ module.exports = {
     webpack: {
         alias: {
             '@api': path.resolve(__dirname, 'src/api'),
-            '@components': path.resolve(__dirname, 'src/components'),
             '@hooks': path.resolve(__dirname, 'src/hooks'),
             '@models': path.resolve(__dirname, 'src/models'),
-            '@pages': path.resolve(__dirname, 'src/pages'),
-            '@router': path.resolve(__dirname, 'src/router'),
-            '@styles': path.resolve(__dirname, 'src/components'),
             '@utils': path.resolve(__dirname, 'src/utils'),
+
+            '@atoms': path.resolve(__dirname, 'src/components/atoms'),
+            '@molecules': path.resolve(__dirname, 'src/components/molecules'),
+            '@organisms': path.resolve(__dirname, 'src/components/organisms'),
+            '@templates': path.resolve(__dirname, 'src/components/templates'),
+            '@pages': path.resolve(__dirname, 'src/components/pages'),
+            '@router': path.resolve(__dirname, 'src/components/router'),
         }
     },
 };
