f2912e225c3935409a7e7f9cfd035cc9d701a30b
AlexeySyantovich <syantovich.a.s@gmail.com>
2022-09-02 11:52:02 +0300

    fix: 'ReactDOM' is defined but never used

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import React, { StrictMode } from "react";
-import ReactDOM, { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const root = createRoot(
-    document.getElementById("root") as HTMLElement
-);
+const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-    <StrictMode>
-        <App />
-    </StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
