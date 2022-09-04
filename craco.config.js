7fef5f6f3ecd48e9764dfa589626df6555cbaec1
Oliver Tears <olivertears@gmail.com>
2022-09-04 12:28:51 +0300

    build: prettier added + changed esling arbnb plugin to typescript recommended

--- craco.config.js
+++ craco.config.js
@@ -1,19 +1,19 @@
-import path from 'path'
+const path = require("path");
 
 module.exports = {
-    webpack: {
-        alias: {
-            '@api': path.resolve(__dirname, 'src/api'),
-            '@hooks': path.resolve(__dirname, 'src/hooks'),
-            '@models': path.resolve(__dirname, 'src/models'),
-            '@utils': path.resolve(__dirname, 'src/utils'),
+  webpack: {
+    alias: {
+      "@api": path.resolve(__dirname, "src/api"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@models": path.resolve(__dirname, "src/models"),
+      "@utils": path.resolve(__dirname, "src/utils"),
 
-            '@atoms': path.resolve(__dirname, 'src/components/atoms'),
-            '@molecules': path.resolve(__dirname, 'src/components/molecules'),
-            '@organisms': path.resolve(__dirname, 'src/components/organisms'),
-            '@templates': path.resolve(__dirname, 'src/components/templates'),
-            '@pages': path.resolve(__dirname, 'src/components/pages'),
-            '@router': path.resolve(__dirname, 'src/components/router'),
-        }
+      "@atoms": path.resolve(__dirname, "src/components/atoms"),
+      "@molecules": path.resolve(__dirname, "src/components/molecules"),
+      "@organisms": path.resolve(__dirname, "src/components/organisms"),
+      "@templates": path.resolve(__dirname, "src/components/templates"),
+      "@pages": path.resolve(__dirname, "src/components/pages"),
+      "@router": path.resolve(__dirname, "src/components/router"),
     },
+  },
 };
