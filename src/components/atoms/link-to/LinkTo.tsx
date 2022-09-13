0399e5915a4d0895565e9de1924c28b940f65abf
syantovich <72738946+syantovich@users.noreply.github.com>
2022-09-13 17:12:37 +0300

    fix:add props to my link

--- src/components/atoms/link-to/LinkTo.tsx
+++ src/components/atoms/link-to/LinkTo.tsx
@@ -2,13 +2,10 @@ import React, { FC } from 'react';
 import { Link as LinkRouter, LinkProps as ReactRouterLinkProps } from 'react-router-dom';
 import { Link, LinkProps as MuiLinkProps } from '@mui/material';
 
-export const LinkTo: FC<MuiLinkProps & ReactRouterLinkProps> = ({
-  children,
-  to
-}: ReactRouterLinkProps & MuiLinkProps) => {
+export const LinkTo: FC<MuiLinkProps & ReactRouterLinkProps> = (props: ReactRouterLinkProps & MuiLinkProps) => {
   return (
-    <Link component={LinkRouter} to={`/${to}`}>
-      {children}
+    <Link component={LinkRouter} {...props} to={`/${props.to}`}>
+      {props.children}
     </Link>
   );
 };
