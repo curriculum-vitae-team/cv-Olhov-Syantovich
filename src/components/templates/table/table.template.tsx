45ec94230f7e8cdaf38c8e3dab8300c1ed5adaff
syantovich <72738946+syantovich@users.noreply.github.com>
2022-10-03 14:56:05 +0300

    fix: delete context

--- src/components/templates/table/table.template.tsx
+++ src/components/templates/table/table.template.tsx
@@ -1,10 +1,9 @@
-import { memo, useMemo, useState, useDeferredValue, FC, useCallback, ChangeEvent } from 'react';
+import { memo, useState, useDeferredValue, FC, useCallback, ChangeEvent } from 'react';
 import { Table, TableBody, TableHead } from '@mui/material';
 import { SortOrder } from '@constants/sort-order.constant';
 import { handleSearch as handleSearchHelpers } from '@helpers/table-search.helper';
 import { handleOrder, handleSort as handleSortHelpers } from '@helpers/table-sort.helper';
 import { Item, TableProps } from './table.types';
-import { TableContext } from './table.context';
 import { Path } from 'react-hook-form';
 
 const TableTemplate = <T extends Item>({
@@ -25,9 +24,6 @@ const TableTemplate = <T extends Item>({
     setOrder(handleOrder);
   }, []);
 
-  const value = useMemo(() => {
-    return { search, sortBy, order, setSearch, setSortBy, toggleOrder };
-  }, [search, sortBy, order, toggleOrder]);
   const handleSearch = useCallback(
     (event: ChangeEvent<HTMLInputElement>) => {
       setSearch && setSearch(event.target.value);
@@ -44,25 +40,23 @@ const TableTemplate = <T extends Item>({
   );
   return (
     <Table stickyHeader sx={{ mb: 3 }}>
-      <TableContext.Provider value={value as never}>
-        <TableHead>
-          <TableHeadComponent
-            search={search}
-            sortBy={sortBy}
-            order={order}
-            handleSort={handleSort}
-            handleSearch={handleSearch}
-          />
-        </TableHead>
-        <TableBody>
-          {items
-            .filter(handleSearchHelpers<T>(searchKeys, deferredSearch))
-            .sort(handleSortHelpers<T>(deferredSortBy, deferredOrder))
-            .map((item) => (
-              <TableRowComponent key={item.id} item={item} />
-            ))}
-        </TableBody>
-      </TableContext.Provider>
+      <TableHead>
+        <TableHeadComponent
+          search={search}
+          sortBy={sortBy}
+          order={order}
+          handleSort={handleSort}
+          handleSearch={handleSearch}
+        />
+      </TableHead>
+      <TableBody>
+        {items
+          .filter(handleSearchHelpers<T>(searchKeys, deferredSearch))
+          .sort(handleSortHelpers<T>(deferredSortBy, deferredOrder))
+          .map((item) => (
+            <TableRowComponent key={item.id} item={item} />
+          ))}
+      </TableBody>
     </Table>
   );
 };
